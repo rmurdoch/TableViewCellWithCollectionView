b1f42ca2e8fcbf332b7059199f73689589a67b70
rmurdoch <rmurdoch4@gmail.com>
2014-03-14 10:52:57 -0400

    TableViewCellCollectionView
    Initial Commit

---
+++ TableViewCellWithCollectionView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewCellWithCollectionView
+//
+//  Created by Andrew Murdoch on 3/14/14.
+//  Copyright (c) 2014 Andrew Murdoch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TableViewCellWithCollectionViewTests/TableViewCellWithCollectionViewTests.m
@@ -0,0 +1,34 @@
+//
+//  TableViewCellWithCollectionViewTests.m
+//  TableViewCellWithCollectionViewTests
+//
+//  Created by Andrew Murdoch on 3/14/14.
+//  Copyright (c) 2014 Andrew Murdoch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewCellWithCollectionViewTests : XCTestCase
+
+@end
+
+@implementation TableViewCellWithCollectionViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
